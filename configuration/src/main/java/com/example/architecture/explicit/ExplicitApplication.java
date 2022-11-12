82197dcfc4e810918fc581a086a0ef67bc436053
zhouqiang <2902511578@qq.com>
2022-11-12 15:48:42 +0800

    Add Explicit-Architecture example module

---
+++ configuration/src/main/java/com/example/architecture/explicit/ExplicitApplication.java
@@ -0,0 +1,13 @@
+package com.example.architecture.explicit;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplicitApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExplicitApplication.class, args);
+	}
+
+}
---
+++ configuration/src/test/java/com/example/architecture/explicit/ExplicitApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.architecture.explicit;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ExplicitApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
